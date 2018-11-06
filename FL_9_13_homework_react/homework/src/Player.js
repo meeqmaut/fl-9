6cf1d3cf85cfd95bd960af537966c9e4fc4943ac
Victor Levkovich <victoriovincitore@gmail.com>
2018-11-06 22:04:33 +0200

    Add files via upload

---
+++ FL_9_13_homework_react/homework/src/Player.js
@@ -0,0 +1,95 @@
+// import React, { Component } from 'react';
+//
+// import { Playlist } from './Playlist';
+//
+// class Player extends Component {
+//  render() {
+//    return (
+//      <div className="wrapper">
+//        <div className="player">
+//          <h2>Now playing</h2>
+//          <div className="panel">
+//            <audio src="" />
+//            <div className="player-info">
+//              <img src=" " alt="#" />
+//              <div className="name">All of me</div>
+//              <div className="name">John Legend</div>
+//            </div>
+//            <div className="controls">
+//              <div className="progress">
+//                <div className="progress-line" />
+//              </div>
+//              <div className="btns">
+//                <i className="material-icons prev">skip_previous</i>
+//                <i className="material-icons stop">play_arrow</i>
+//                <i className="material-icons next">skip_next</i>
+//              </div>
+//            </div>
+//          </div>
+//        </div>
+//        <Playlist />
+//      </div>
+//    );
+//  }
+// }
+//
+// export { Player };
+import React, { Component } from 'react';
+
+import { Playlist } from './Playlist';
+
+
+class Player extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.getTrack = this.getTrack.bind(this);
+  }
+
+  getTrack(value) {
+    const {
+      author, mp3, poster, title,
+    } = value;
+    this.setState({
+      author,
+      mp3,
+      poster,
+      title,
+    });
+  }
+
+  render() {
+    const {
+      author, mp3, poster, title,
+    } = this.state;
+    return (
+      <div className="wrapper">
+        <div>
+          <h2>Now playing</h2>
+          <div className="player">
+            <audio autoPlay src={mp3} />
+            <div className="panel">
+              <img src={poster} alt="" />
+              <div className="author">{ author }</div>
+              <div className="name">{ title }</div>
+            </div>
+            <div className="controls">
+              <div className="progress">
+                <div className="progress-line" />
+              </div>
+              <div className="btns">
+                <i className="material-icons prev">skip_previous</i>
+                <i className="material-icons stop">play_arrow</i>
+                <i className="material-icons next">skip_next</i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Playlist getTrack={this.getTrack} />
+      </div>
+    );
+  }
+}
+
+
+export { Player };
